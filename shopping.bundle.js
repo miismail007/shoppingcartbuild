95030ebeecf8cfd8465022ee95cc96d34896989e
Mohammed ismail <87133585+ismail-anywhere@users.noreply.github.com>
2024-09-29 19:06:20 +0530

    root issue fix

--- shopping.bundle.js
+++ shopping.bundle.js
@@ -42,12 +42,12 @@ function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
 var products = [{
   id: 1,
   name: "Mac Book Air",
-  price: "58,990",
+  price: 58990,
   image: "https://cdsassets.apple.com/live/SZLF0YNV/images/sp/111883_macbookair.png"
 }, {
   id: 2,
   name: "IPhone 16",
-  price: "79,900",
+  price: 79900,
   image: "https://www.apple.com/newsroom/images/2024/09/apple-introduces-iphone-16-and-iphone-16-plus/article/geo/Apple-iPhone-16-hero-geo-240909_inline.jpg.large.jpg"
 }
 // More products
