76b1a41bb842673a8036ac554a302c4ac2d5e368
Mohammed ismail <87133585+ismail-anywhere@users.noreply.github.com>
2024-09-29 09:15:38 +0530

    root to window

--- shopping.bundle.js
+++ shopping.bundle.js
@@ -53,11 +53,10 @@ function Index() {
 function mountShopping() {
   var includeInDom = _export__WEBPACK_IMPORTED_MODULE_2__["default"];
   var targetElement = document.querySelector("#shoppingCart");
-  var root;
-  if (!root) {
-    root = (0,react_dom_client__WEBPACK_IMPORTED_MODULE_1__.createRoot)(targetElement);
+  if (!window.shoppingRoot) {
+    window.shoppingRoot = (0,react_dom_client__WEBPACK_IMPORTED_MODULE_1__.createRoot)(targetElement);
   }
-  root.render(/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(Index, {}));
+  window.shoppingRoot.render(/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(Index, {}));
 }
 
 /***/ }),
